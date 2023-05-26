a97331d292e980cea763d12bed41f31422f7d82d
Erfan Fathi <fathierfan97@gmail.com>
2023-05-26 15:42:31 +0200

    #clean

--- q_table.py
+++ q_table.py
@@ -49,25 +49,3 @@ def discretize_state(state_space, bins):
         state_discrete[i] = np.digitize(state_space[i], bins[i])
 
     return state_discrete.astype(np.int32)
-
-# Unit test
-# def test():
-#     import gym
-#     env = gym.make('CartPole-v1')
-#     # test the q-table
-#     q_table, bins = Qtable(env.observation_space, env.action_space)
-#     print(q_table.shape)
-
-#     # test the discretize_state function
-#     state, _ = env.reset()
-#     state_discrete = discretize_state(state, bins)
-#     print(state_discrete)
-
-#     # sample step
-#     action = env.action_space.sample()
-#     next_state = None
-#     for i in range(100):
-#         next_state, reward, done = env.step(action)[:3]
-
-#     state_next_discrete = discretize_state(next_state, bins)
-#     print(state_next_discrete)
